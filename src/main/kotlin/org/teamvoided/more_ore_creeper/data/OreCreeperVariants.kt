e536e2a4deab5a848fee6f0813bc2a7cc842d091
TheEnderCore <theendercore@gmail.com>
2025-10-20 22:25:49 +0300

    RANDOMIUM

--- src/main/kotlin/org/teamvoided/more_ore_creeper/data/OreCreeperVariants.kt
+++ src/main/kotlin/org/teamvoided/more_ore_creeper/data/OreCreeperVariants.kt
@@ -8,8 +8,7 @@ import org.teamvoided.more_ore_creeper.init.MOCRegistries.ORE_CREEPER_VARIANT
 object OreCreeperVariants {
     val DEFAULT = create("default")
 
-
-    val EVIL = create("evil")
+    val RANDOMIUM = create("randomium")
 
     fun create(path: String): ResourceKey<OreCreeperVariant> = ResourceKey.create(ORE_CREEPER_VARIANT, id(path))
 }
--- src/main/kotlin/org/teamvoided/more_ore_creeper/data/gen/prov/OreCreeperVariantProv.kt
+++ src/main/kotlin/org/teamvoided/more_ore_creeper/data/gen/prov/OreCreeperVariantProv.kt
@@ -1,6 +1,7 @@
 package org.teamvoided.more_ore_creeper.data.gen.prov
 
 
+import net.mehvahdjukaar.randomium.Randomium
 import net.minecraft.core.Holder
 import net.minecraft.core.particles.ColorParticleOption
 import net.minecraft.core.registries.Registries
@@ -10,10 +11,12 @@ import net.minecraft.resources.ResourceLocation
 import net.minecraft.tags.BiomeTags
 import net.minecraft.tags.BlockTags
 import net.minecraft.tags.TagKey
+import net.minecraft.util.random.SimpleWeightedRandomList
 import net.minecraft.world.level.biome.Biome
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
-import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider
+import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.level.levelgen.feature.stateproviders.WeightedStateProvider
 import org.teamvoided.more_ore_creeper.MoreOreCreeper.id
 import org.teamvoided.more_ore_creeper.data.OreCreeperVariants
 import org.teamvoided.more_ore_creeper.entity.OreCreeperVariant
@@ -22,18 +25,31 @@ import org.teamvoided.more_ore_creeper.init.MOCParticleTypes
 
 object OreCreeperVariantProv {
     val MISSING = texture("mising")
-    val tempTexture = id("ore_creeper", "entity/coal_ore_creeper")
+    val tempTexture = id("ore_creeper", "entity/coal_creeper")
     const val RADIUS = 3.75f
 
     fun bootstrap(c: BootstrapContext<OreCreeperVariant>) {
         c.registerDefault(OreCreeperVariants.DEFAULT)
         c.register(
-            OreCreeperVariants.EVIL,
+            OreCreeperVariants.RANDOMIUM,
             BiomeTags.IS_OVERWORLD,
-            MISSING,
+            tempTexture,
             listOf(color(.23f, 1f, 0f)),
             RADIUS,
-            listOf(c.placement(BlockTags.DIRT, Blocks.END_STONE))
+            listOf(
+                c.placement(
+                    BlockTags.STONE_ORE_REPLACEABLES,
+                    Randomium.RANDOMIUM_ORE.get() to 6, Blocks.AIR to 4,
+                ),
+                c.placement(
+                    BlockTags.DEEPSLATE_ORE_REPLACEABLES,
+                    Randomium.RANDOMIUM_ORE_DEEP.get() to 6, Blocks.AIR to 4,
+                ),
+                c.placement(
+                    BlockTags.INFINIBURN_END,
+                    Randomium.RANDOMIUM_ORE_END.get() to 6, Blocks.AIR to 4,
+                )
+            )
         )
     }
 
@@ -44,7 +60,12 @@ object OreCreeperVariantProv {
             tempTexture,
             listOf(color(.5f, .5f, .5f)),
             RADIUS,
-            listOf(placement(BlockTags.STONE_ORE_REPLACEABLES, Blocks.DIAMOND_BLOCK))
+            listOf(
+                placement(
+                    BlockTags.STONE_ORE_REPLACEABLES,
+                    Blocks.DIAMOND_BLOCK to 1, Blocks.DIAMOND_ORE to 6, Blocks.AIR to 3,
+                )
+            )
         )
     }
 
@@ -62,8 +83,21 @@ object OreCreeperVariantProv {
         )
     }
 
-    fun BootstrapContext<OreCreeperVariant>.placement(tag: TagKey<Block>, block: Block) =
-        OrePlacement(lookup(Registries.BLOCK).getOrThrow(tag), BlockStateProvider.simple(block))
+
+    fun BootstrapContext<OreCreeperVariant>.placement(tag: TagKey<Block>, vararg blocks: Pair<Block, Int>)
+            : OrePlacement =
+        statePlacement(tag, *blocks.map { it.first.defaultBlockState() to it.second }.toTypedArray())
+
+    fun BootstrapContext<OreCreeperVariant>.statePlacement(
+        tag: TagKey<Block>, vararg blocks: Pair<BlockState, Int>,
+    ): OrePlacement {
+        val list = SimpleWeightedRandomList.Builder<BlockState>()
+        for ((block, weight) in blocks) {
+            list.add(block, weight)
+        }
+
+        return OrePlacement(lookup(Registries.BLOCK).getOrThrow(tag), WeightedStateProvider(list))
+    }
 
     fun color(r: Float, g: Float, b: Float): ColorParticleOption =
         ColorParticleOption.create(MOCParticleTypes.COLORED_EXPLOSION, r, g, b)
