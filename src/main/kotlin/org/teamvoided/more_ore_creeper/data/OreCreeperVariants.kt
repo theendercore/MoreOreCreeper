48929a5cfae4f91e05a797078d042d30b4f9ee05
TheEnderCore <theendercore@gmail.com>
2025-10-21 15:14:25 +0300

    built in load conditions

--- src/main/kotlin/org/teamvoided/more_ore_creeper/data/OreCreeperVariants.kt
+++ src/main/kotlin/org/teamvoided/more_ore_creeper/data/OreCreeperVariants.kt
@@ -8,7 +8,9 @@ import org.teamvoided.more_ore_creeper.init.MOCRegistries.ORE_CREEPER_VARIANT
 object OreCreeperVariants {
     val DEFAULT = create("default")
 
-    val RANDOMIUM = create("randomium")
+    val RANDOMIUM = create("randomium", "randomium")
 
     fun create(path: String): ResourceKey<OreCreeperVariant> = ResourceKey.create(ORE_CREEPER_VARIANT, id(path))
+    fun create(namespace: String, path: String): ResourceKey<OreCreeperVariant> =
+        ResourceKey.create(ORE_CREEPER_VARIANT, id(namespace, path))
 }
--- src/main/kotlin/org/teamvoided/more_ore_creeper/data/gen/MoreOreCreeperData.kt
+++ src/main/kotlin/org/teamvoided/more_ore_creeper/data/gen/MoreOreCreeperData.kt
@@ -4,9 +4,14 @@ import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricDynamicRegistryProvider
+import net.fabricmc.fabric.api.resource.conditions.v1.ResourceConditions
 import net.minecraft.core.HolderLookup
+import net.minecraft.core.HolderLookup.RegistryLookup
 import net.minecraft.core.RegistrySetBuilder
+import org.teamvoided.more_ore_creeper.MoreOreCreeper.MODID
 import org.teamvoided.more_ore_creeper.MoreOreCreeper.log
+import org.teamvoided.more_ore_creeper.data.gen.prov.BiomeTagsProvider
+import org.teamvoided.more_ore_creeper.data.gen.prov.BlockTagsProvider
 import org.teamvoided.more_ore_creeper.data.gen.prov.OreCreeperVariantProv
 import org.teamvoided.more_ore_creeper.init.MOCRegistries
 import java.util.concurrent.CompletableFuture
@@ -17,6 +22,8 @@ object MoreOreCreeperData : DataGeneratorEntrypoint {
         log.info("Hello from DataGen")
         val pack = gen.createPack()
 
+        pack.addProvider(::BlockTagsProvider)
+        pack.addProvider(::BiomeTagsProvider)
         pack.addProvider(::DynRegProvider)
     }
 
@@ -28,7 +35,14 @@ object MoreOreCreeperData : DataGeneratorEntrypoint {
         FabricDynamicRegistryProvider(o, r) {
         override fun getName(): String = "more_ore_creepers"
         override fun configure(p: HolderLookup.Provider, e: Entries) {
-            e.addAll(p.lookupOrThrow(MOCRegistries.ORE_CREEPER_VARIANT))
+            e.addAllConditional(p.lookupOrThrow(MOCRegistries.ORE_CREEPER_VARIANT))
+        }
+
+        fun <T> Entries.addAllConditional(registry: RegistryLookup<T>) {
+            for (key in registry.listElementIds()) {
+                if (key.location().namespace == MODID) add(registry, key)
+                else add(registry, key, ResourceConditions.allModsLoaded(key.location().namespace))
+            }
         }
     }
 }
