749863d2eb8fbae28df398900c71241c76b88ec6
TheEnderCore <theendercore@gmail.com>
2025-10-24 13:47:40 +0300

    removed debug command

--- src/main/kotlin/org/teamvoided/more_ore_creeper/MoreOreCreeper.kt
+++ src/main/kotlin/org/teamvoided/more_ore_creeper/MoreOreCreeper.kt
@@ -1,21 +1,11 @@
 package org.teamvoided.more_ore_creeper
 
-import com.mojang.brigadier.context.CommandContext
 import me.fzzyhmstrs.fzzy_config.api.ConfigApi
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
-import net.minecraft.commands.CommandSourceStack
-import net.minecraft.commands.Commands.argument
-import net.minecraft.commands.Commands.literal
-import net.minecraft.commands.arguments.ResourceArgument
-import net.minecraft.core.Holder
 import net.minecraft.resources.ResourceLocation
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.teamvoided.more_ore_creeper.config.MOCConfig
-import org.teamvoided.more_ore_creeper.entity.ModdedOreCreeper
-import org.teamvoided.more_ore_creeper.entity.variant.OreCreeperVariant
 import org.teamvoided.more_ore_creeper.init.*
-import org.teamvoided.more_ore_creeper.init.MOCRegistries.ORE_CREEPER_VARIANT
 
 @Suppress("unused")
 object MoreOreCreeper {
@@ -35,25 +25,6 @@ object MoreOreCreeper {
         MOCEntityTypes.init()
         MOCItems.init()
 
-        CommandRegistrationCallback.EVENT.register { dispatcher, ctx, _ ->
-            val creeper = literal("creeper").build()
-            dispatcher.root.addChild(creeper)
-            val cVariant = argument("variant", ResourceArgument.resource(ctx, ORE_CREEPER_VARIANT))
-                .executes { spawnCreeper(it, ResourceArgument.getResource(it, "variant", ORE_CREEPER_VARIANT)) }
-                .build()
-            creeper.addChild(cVariant)
-        }
-    }
-
-    fun spawnCreeper(context: CommandContext<CommandSourceStack>, resource: Holder.Reference<OreCreeperVariant>): Int {
-        val src = context.source ?: return -1
-        val level = src.level ?: return -1
-        val pos = src.position ?: return -1
-        val creeper = ModdedOreCreeper(level)
-        creeper.setPos(pos)
-        creeper.variant = resource
-        level.addFreshEntity(creeper)
-        return 0
     }
 
     fun id(namespace: String, path: String): ResourceLocation = ResourceLocation.fromNamespaceAndPath(namespace, path)
